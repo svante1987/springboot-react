7d759195c1c27177b55c93e37df66d065b3fbb45
Svante Pålsson <svantepalsson1987@gmail.com>
2022-09-20 22:02:07 +0200

    made a simple table

---
+++ src/components/employee/ListEmployee.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ListEmployee = (props) => {
+
+  
+
+  return (
+    <div>
+      <h2>Employee List</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Employee First Name</th>
+              <th>Employee Last Name</th>
+              <th>Employee Email Id</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+
+            }
+          </tbody>
+
+        </table>
+
+    </div>
+  )
+}
+
+export default ListEmployee
